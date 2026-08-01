3852d7d6852623a15c2523c233e99b0d599005d8
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2026-08-02 01:33:30 +0200

    Deployed fdb26db with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,22 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  if (typeof document$ !== "undefined") {
+    document$.subscribe(() => {
+      if (window.MathJax && MathJax.typesetPromise) {
+        MathJax.typesetClear();
+        MathJax.texReset();
+        MathJax.typesetPromise();
+      }
+    });
+  }
